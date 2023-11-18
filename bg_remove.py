b054f3e7e862ee5dc6c66bed7f7ce96afce94735
lampongyuen <46077833+lampongyuen@users.noreply.github.com>
2023-11-18 17:10:08 +0800

    Update bg_remove.py

--- bg_remove.py
+++ bg_remove.py
@@ -10,7 +10,7 @@ st.write("## Remove background from your image")
 st.write(
     ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
 )
-st.sidebar.write("## Upload and download :gear:")
+st.sidebar.write("## Please upload an image for download after removing background :gear:")
 
 MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
 
